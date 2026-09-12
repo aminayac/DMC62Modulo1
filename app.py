009ce74986087a2d3750464a86609542064e3e91
aminayac <aminayac@gmail.com>
2026-09-12 18:56:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("Especialización e Python for Analytics")
+st.title("Especialización en Python for Analytics")
+st.image("Python-logo.png",width =400)
 st.subheader("Módulo 1")
 st.write("Elaborado por Anibal Minaya")
 st.markdown("prueba") 
