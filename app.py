0750b2150d1ed45f1f1d387f423f441850ea7a4b
aminayac <aminayac@gmail.com>
2026-09-12 18:57:14 -0500

    Fix image filename case in app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 st.title("Especialización en Python for Analytics")
-st.image("Python-logo.png",width =400)
+st.image("python-logo.png",width =400)
 st.subheader("Módulo 1")
 st.write("Elaborado por Anibal Minaya")
 st.markdown("prueba") 
