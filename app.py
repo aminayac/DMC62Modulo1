457c0e8aa5549e9fa790cf63605fc3c2301ba486
aminayac <aminayac@gmail.com>
2026-09-12 19:06:39 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,15 @@ import streamlit as st
 
 st.title("Especialización en Python for Analytics")
 st.image("python-logo.png",width =400)
-st.subheader("Módulo 1")
-st.write("Elaborado por Anibal Minaya")
-st.markdown("prueba") 
+opcion = st.selectbox(
+    "Seleccione una opción",
+    ("Home", "Módulo 1", "Módulo 2", "Módulo 3", "Módulo 4"),
+    index=0
+)
+
+if opcion == "Home":
+  #página home de preseentación del proyecto
+  st.subheader("Módulo 1")
+  st.write("Elaborado por Anibal Minaya")
+  st.markdown("prueba") 
+
