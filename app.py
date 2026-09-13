3eb2d890cc491d253f564c4f0b73f04e58fddfaf
aminayac <aminayac@gmail.com>
2026-09-12 19:30:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ import streamlit as st
 
 st.title("Especialización en Python for Analytics",text_alignment="center")
 
-# Crea tres columnas
+#crea tres columnas para mostrar la imagen centrada
 col1, col2, col3 = st.columns(3)
-# Coloca la imagen en la columna del medio
+#coloca la imagen en la columna del medio
 with col2:
     st.image("python-logo.png")
-    
-#st.image("python-logo.png")
+
+#en la barra lateral izq muestra la imagen y crea el select box
 st.sidebar.image("python-logo.png")
 opcion = st.sidebar.selectbox(
     "Seleccione una opción",
@@ -18,7 +18,11 @@ opcion = st.sidebar.selectbox(
 
 if opcion == "Home":
   #página home de presentación del proyecto
-  st.subheader("Módulo 1")
-  st.write("Elaborado por Anibal Minaya")
-  st.markdown("prueba") 
+  #st.subheader("Módulo 1")
+  st.write("Elaborado por Anibal Abraham Minaya Cubillas")
+  st.write("Módulo 1")
+  st.write("Información general del estudiante")  
+  st.write("Año : ")
+  st.write("Breve descripción del proyecto")
+  st.write("Tecnologías utilizadas")
 
