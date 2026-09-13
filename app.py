77239ac942d2ca000a1a21bfe3eb99a4537a0110
aminayac <aminayac@gmail.com>
2026-09-12 19:46:19 -0500

    Refactor app.py for improved structure and clarity

--- app.py
+++ app.py
@@ -10,8 +10,9 @@ with col2:
 
 #en la barra lateral izq muestra la imagen y crea el select box
 st.sidebar.image("python-logo.png")
+st.sidebar.markdown(""###Seleccione una opción")
 opcion = st.sidebar.selectbox(
-    "###Seleccione una opción",
+    "",
     ("Home", "Ejercicio 1", "Ejercicio 2", "Ejercicio 3", "Ejercicio 4"),
     index=0
 )
