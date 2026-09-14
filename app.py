fb32da2d3719aeabc90e08af0628f1f3664b9021
aminayac <aminayac@gmail.com>
2026-09-13 20:05:38 -0500

    Refactor app.py to restore imports and structure

--- app.py
+++ app.py
@@ -92,8 +92,8 @@ elif opcion == EJERCICIO1:
       #st.write(lista_tipos_mov)
       #st.write(lista_valores)
   diccionario_datos = {"Concepto" : lista_conceptos, "Tipo Movimiento" : lista_tipos_mov, "Valor" : lista_valores}
-  tabla = pandas.DataFrame(diccionario_datos)
-  st.write(tabla, index=None)
+  tabla = pandas.DataFrame(diccionario_datos, index=None)
+  st.write(tabla)
   col1.text_input.value = ""
   col2.selectbox.index=None
   col3.number_input.value=0
