843d7c96677d7eb518031f9426ae86fe29685e57
aminayac <aminayac@gmail.com>
2026-09-14 22:11:26 -0500

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -89,7 +89,7 @@ elif opcion == EJERCICIO1:
   st.markdown("**Ejercicio 1 – Flujo de caja con listas**")
   st.markdown("En este ejercicio se desarrolla un módulo para registrar movimientos financieros en una lista vacía.")
   #crea 4 columnas para mostrar los elementos del formulario en forma horizontal
-  col1, col2, col3, col4 = st.columns(4, width=1200)
+  col1, col2, col3, col4 = st.columns(4, width=2200)
 
   concepto = col1.text_input("Concepto")
   tipo_mov = col2.selectbox("Tipo Movimiento",("Ingreso","Gasto"),index=None,placeholder="Seleccione...")
