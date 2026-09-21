065373d2b74c5ee0c4e47bfe9f72d2a7a6b9673b
aminayac <aminayac@gmail.com>
2026-09-20 23:53:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import zoneinfo
 import pandas as pd
 import numpy as np
 import libreria_funciones_proyecto1 as lfp1
+import librería_clases_proyecto1 as lcp1
+import libreria_gestor_clases as lgc
 
 #funcion para el ejercicio 1, para obtener totales de ingresos y gastos
 def obtener_totales(lista):
@@ -31,6 +33,7 @@ FUNCION_ERROR_TXS = "Tasa de error de transacciones"
 # Configuración de la página
 st.set_page_config(
     page_title="Especialización en Python for Analytics - Módulo 1",
+    layout="wide",
     page_icon=":armenia:"  #mis iniciales AM
 )
 
@@ -39,10 +42,10 @@ st.title("Especialización en Python for Analytics",text_alignment="center")
 #crea tres columnas para mostrar la imagen centrada
 col1, col2, col3 = st.columns(3)
 #coloca la imagen en la columna del medio
-col2.image("python-logo.png")
+col2.image("img/python-logo.png")
 
 #en la barra lateral izq muestra la imagen y crea el select box
-st.sidebar.image("python-logo.png")
+st.sidebar.image("img/python-for-analytics.png")
 opcion = st.sidebar.selectbox(
     "**Seleccione una opción**",
     (HOME, EJERCICIO1, EJERCICIO2, EJERCICIO3, EJERCICIO4),
@@ -51,23 +54,35 @@ opcion = st.sidebar.selectbox(
 
 if opcion == HOME:
   #página home de presentación del proyecto
-  #st.subheader("Módulo 1")
-  st.write("Elaborado por Anibal Abraham Minaya Cubillas")
-  st.write("Módulo 1")
-  st.write("Información general del estudiante")  
-  st.write(f"Año: {datetime.datetime.now().year}")
-  st.write("Breve descripción del proyecto")
-  st.write("Tecnologías utilizadas")
+  st.header("👨‍🎓 Elaborado por: ",text_alignment ="center")
+  st.subheader("Anibal Abraham Minaya Cubillas",text_alignment ="center")
+  st.header("Módulo:",text_alignment ="center")
+  st.subheader("Módulo 1: Python Fundamentals",text_alignment ="center")
+  st.header("Información general del estudiante:",text_alignment ="center")
+  st.subheader("Me he desempeñado como Developer JAVA y Developer Cobol",text_alignment ="center")
+  st.header("Año:",text_alignment ="center")
+  st.subheader(f"{datetime.datetime.now().year}",text_alignment ="center")
+  st.header("Breve descripción del proyecto:",text_alignment ="center")
+  st.subheader("Aplicación interactiva en Streamlit que integra los conceptos",text_alignment ="center")
+  st.subheader("aprendidos durante el Módulo 1 del curso, que incluyen, entre otros,",text_alignment ="center")
+  st.subheader("el uso de listas, arrays, DataFrames, funciones y clases (POO)",text_alignment ="center")          
+  st.write("")
+  st.write("")
+  st.header("Tecnologías utilizadas:",text_alignment ="center")
+  col1, col2, col3 = st.columns(3)
+  col1.image("img/python-logo.png")
+  col2.image("img/streamlit.png")
+  col3.image("img/github.png")
 elif opcion == EJERCICIO1:
   #Ejercicio 1 – Flujo de caja con listas
   #inicializa listas
   lista_conceptos = []
   lista_tipos_mov = []
   lista_valores   = []
-  """
-  Verifica si existen las variables de sesion: lista de conceptos, tipos y montos
-  si no existen, las inicializa, si existen, las recupera
-  """
+  
+  #Verifica si existen las variables de sesion: lista de conceptos, tipos y montos
+  #si no existen, las inicializa, si existen, las recupera
+  
   if 'LISTA_CONCEPTOS' not in st.session_state:
     st.session_state['LISTA_CONCEPTOS'] = lista_conceptos
     st.session_state['LISTA_TIPO_MOV'] = lista_tipos_mov
@@ -77,14 +92,14 @@ elif opcion == EJERCICIO1:
     lista_tipos_mov = st.session_state['LISTA_TIPO_MOV']
     lista_valores   = st.session_state['LISTA_VALOR']
 
-  st.markdown("**Ejercicio 1 – Flujo de caja con listas**")
-  st.markdown("En este ejercicio se desarrolla un módulo para registrar movimientos financieros en una lista vacía.")
+  st.header("🏦 Ejercicio 1 – Flujo de caja con listas")
+  st.markdown("**En este ejercicio se desarrolla un módulo para registrar movimientos financieros en una lista vacía.**")
   #crea 4 columnas para mostrar los elementos del formulario en forma horizontal
   col1, col2, col3, col4 = st.columns([2, 1, 1, 1])
 
   concepto = col1.text_input("Concepto")
   tipo_mov = col2.selectbox("Tipo Movimiento",("Ingreso","Gasto"),index=None,placeholder="Seleccione...")
-  valor = round(col3.number_input("Valor S/",value=0.00,format="%.2f"),2)
+  valor = round(col3.number_input("Valor S/",value=0.00,min_value=0.00,format="%.2f"),2)
   col4.write("")
   col4.write("")
   if col4.button("Añadir",type="primary"):
@@ -114,16 +129,16 @@ elif opcion == EJERCICIO1:
   st.dataframe(df, hide_index=True,column_config={"Valor": st.column_config.NumberColumn("Valor", format="S/ %.2f")})
   totales = obtener_totales(lista_valores)
   col1, col2, col3, col4 = st.columns(4)
-  col1.write(f"Ingresos : {totales["Ingresos"]}")
-  col2.write(f"Gastos   : {totales["Gastos"]}")
+  col1.metric("Ingresos", totales["Ingresos"], format="S/ %.2f")
+  col2.metric("Gastos", totales["Gastos"], format="S/ %.2f")
   saldo = totales["Ingresos"] - totales["Gastos"]
-  col3.write(f"Saldo   :  {saldo}")
+  col3.metric("Saldo", saldo, format="S/ %.2f")
   if saldo > 0:
-     col4.metric("Saldo","A favor",delta=saldo,label_visibility="hidden")
+     col4.metric("Caja","A favor",delta=saldo, format="S/ %.2f")
   elif saldo < 0:
-     col4.metric("Saldo","En contra",delta=saldo,label_visibility="hidden")
+     col4.metric("Caja","En contra",delta=saldo, format="S/ %.2f")
   else:
-     col4.metric("Saldo","Equilibrio",delta=saldo,label_visibility="hidden")
+     col4.metric("Caja","Equilibrio",delta=saldo, format="S/ %.2f")
 elif opcion == EJERCICIO2:
   #Ejercicio 2 – Registro con NumPy, arrays y DataFrame
   #inicializa array
@@ -134,15 +149,15 @@ elif opcion == EJERCICIO2:
   else:
     arreglo = st.session_state['ARREGLO']
 
-  st.markdown("**Ejercicio 2 – Registro con NumPy, arrays y DataFrame**")
-  st.markdown("ddfvdfEn este ejercicio se desarrolla un módulo para registrar movimientos financieros en una lista vacía.")
+  st.header("💸 Ejercicio 2 – Registro con NumPy, arrays y DataFrame")
+  st.markdown("**En este ejercicio se desarrolla un formulario para registrar ventas usando arreglos de `NumPy`.**")
   #crea 5 columnas para mostrar los elementos del formulario en forma horizontal
-  col1, col2, col3, col4, col5 = st.columns([2, 1, 1, 1, 1])
+  col1, col2, col3, col4, col5 = st.columns([2, 2, 1, 1, 1])
 
   producto = col1.text_input("Producto")
   categoria = col2.selectbox("Categoria",("Laptop","PC","Celular","Audifono","Tablet","Mouse"),index=None,placeholder="Seleccione...")
-  precio = round(col3.number_input("Precio S/",value=0.00,format="%.2f"),2)
-  cantidad = col4.number_input("Cantidad",value=0)
+  precio = round(col3.number_input("Precio S/",min_value=0.00,value=0.00,format="%.2f"),2)
+  cantidad = col4.number_input("Cantidad",min_value=0,value=0)
   col5.write("")
   col5.write("")
   if col5.button("Añadir",type="primary"):
@@ -169,10 +184,10 @@ elif opcion == EJERCICIO2:
   venta_total = np.sum(arreglo[:, 4].astype(float))
   st.metric("Ventas Totales", f"S/ {venta_total:,.2f}")
 elif opcion == EJERCICIO3:
-  st.markdown("**Ejercicio 3 – Uso de funciones desde una librería externa**")
-  st.markdown("En este ejercicio se invoca una función de la librería libreria_funciones_proyecto1.")
+  st.header("📆 Ejercicio 3 – Uso de funciones desde una librería externa")
+  st.markdown("**En este ejercicio se invoca una función de la librería `libreria_funciones_proyecto1.py`**")
 
-  funcion = st.selectbox("Función",("Tasa de error de transacciones"),index=None,placeholder="Seleccione una función...")
+  funcion = st.selectbox("**Función**",("Tasa de error de transacciones"),index=None,placeholder="Seleccione una función...")
   #inicializa array
   arreglo_historico=np.empty((0, 5), dtype=object)
 
@@ -192,9 +207,7 @@ elif opcion == EJERCICIO3:
           resultado = lfp1.calcular_tasa_error_transacciones(fallidas,totales)
           error = resultado["tasa_error_pct"]
           exito = resultado["tasa_exito_pct"]
-          st.write(f"Tasa de error PCT: {error}")
-          st.write(f"Tasa de éxito PCT: {exito}")
-          st.success("Función ejecutada satisfactoriamente.")
+          st.success(f"Función ejecutada correctamente. **Resultado:** Tasa de error PCT: {error}. Tasa de éxito PCT: {exito}")
        except Exception as e:
           error = "Error"
           exito = "Error"
@@ -206,7 +219,75 @@ elif opcion == EJERCICIO3:
     df = pd.DataFrame(arreglo_historico, columns=["TXs Fallidas", "TXs Totales", "Tasa Error", "Tasa Exito", "Fecha"])
     st.dataframe(df, hide_index=True)
         
-  
-        
-#elif opcion == EJERCICIO4:
+elif opcion == EJERCICIO4:
+    st.header("🏢 Ejercicio 4 – Uso de clases desde una librería externa con CRUD")
+    st.markdown("**En este ejercicio se conecta una clase de la librería `libreria_clases_proyecto1.py`. Clase elegida: `Servidor`**")
+    st.header("🖥️ Gestión de Servidores")
 
+    gestor_serv = lgc.GestorServidores()
+    if 'GESTOR_SERV' not in st.session_state:
+        st.session_state['GESTOR_SERV'] = gestor_serv
+    else:
+        gestor_serv = st.session_state['GESTOR_SERV']
+
+    with st.expander("Agregar Servidor"):
+       nombre = st.text_input("Nombre del servidor")
+       tiempo_total = st.number_input("Tiempo total de operación en horas")
+       tiempo_caida = st.number_input("Tiempo caída en horas")
+       almac_total  = st.number_input("Almacenamiento total en GB")
+       almac_usado  = st.number_input("Almacenamiento usado en GB")
+       if st.button("Agregar Servidor",type="primary"):
+        if nombre.strip() == "":
+            st.error("El campo nombre no puede estar vacío.")
+        elif tiempo_total <= 0:
+            st.error("El campo tiempo total no puede ser menor o igual a cero.")
+        elif almac_total <= 0:
+            st.error("El campo almacenamiento total no puede ser menor o igual a cero.")
+        else:
+            servidor = lcp1.Servidor(nombre,tiempo_total,tiempo_caida,almac_total,almac_usado)
+            try:
+                gestor_serv.crear_servidor(servidor)
+                st.success(f"Servidor {nombre} creado exitosamente.")
+            except ValueError as e:
+                st.error(f"Error: {str(e)}")
+    with st.expander("Actualizar Servidor"):
+       nombre = st.selectbox("Nombre del servidor a actualizar", gestor_serv.mostrar_nombres_servidores(), 
+                             index=None, placeholder="Seleccione...")
+       tiempo_total = st.number_input("Nuevo Tiempo total de operación en horas")
+       tiempo_caida = st.number_input("Nuevo Tiempo caída en horas")
+       almac_total  = st.number_input("Nuevo Almacenamiento total en GB")
+       almac_usado  = st.number_input("Nuevo Almacenamiento usado en GB")
+       if st.button("Actualizar Servidor",type="primary"):
+          if tiempo_total <= 0:
+            st.error("El campo tiempo total no puede ser menor o igual a cero.")
+          elif almac_total <= 0:
+            st.error("El campo almacenamiento total no puede ser menor o igual a cero.")
+          else:
+            for serv in gestor_serv.servidores:
+                if serv.nombre == nombre:
+                    serv.tiempo_total_h = tiempo_total
+                    serv.tiempo_caida_h = tiempo_caida
+                    serv.almacenamiento_total_gb = almac_total
+                    serv.almacenamiento_usado_gb = almac_usado
+                    st.success(f"Servidor {nombre} actualizado exitosamente.")
+                    break
+                else:
+                    st.error(f"Servidor con nombre {nombre} no encontrado.")
+    with st.expander("Eliminar Servidor"):
+       nombre = st.selectbox("Nombre del servidor a eliminar", gestor_serv.mostrar_nombres_servidores(), 
+                                    index=None, placeholder="Seleccione...")
+       if st.button("Eliminar Servidor",type="primary"):
+           try:
+               gestor_serv.eliminar_servidor(nombre)
+               st.success(f"Servidor {nombre} eliminado exitosamente.")
+           except Exception as e:
+               st.error(f"Error: {e}")
+    with st.expander("Ver Servidores"):
+           st.markdown("**Lista de Servidores y resultado de invocación de los métodos de la clase `Servidor`**")
+           array_serv = gestor_serv.mostrar_info_servidores()
+           df = pd.DataFrame(array_serv, columns=["Nombre", "Tiempo Total(hrs)", "Tiempo Caída(hrs)",
+                                                  "Almacenamiento Total(GB)", "Almacenamiento Usado(GB)",
+                                                  "Disponibilidad(%)*",
+                                                  "Uso Almacenamiento(%)*",
+                                                  "Estado*"])
+           st.dataframe(df, hide_index=True)
